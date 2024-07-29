17668f35c5e2f9645c81e86ae4852e7888082ba5
jucastro15 <ra56421610838@acaonsfatima.org.br>
2024-07-29 18:19:32 -0300

    certo

---
+++ src/pages/inicio/index.jsx
@@ -0,0 +1,17 @@
+import "./index.scss"
+
+export default function Inicio() {
+
+
+    return(
+        <div className="pagina-inicio">
+            <header className="cabeçalho"> 
+            <img className="frei" src="/assets/images/logo_frei.png" alt="Frei" />
+            <h1>React FreiS</h1>
+
+            </header>
+
+        </div>
+    );
+    
+}
---
+++ src/router.jsx
@@ -0,0 +1,14 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Inicio from './pages/inicio';
+
+
+export default function Navegacao() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path='/' element={<Inicio />} />
+               
+            </Routes>
+        </BrowserRouter>
+    );
+}
