cd60e68f7ab48faec681fe83c01045768168e04f
jucastro15 <ra56421610838@acaonsfatima.org.br>
2024-07-31 00:37:32 -0300

    funciona

--- src/pages/inicio/index.jsx
+++ src/pages/inicio/index.jsx
@@ -1,17 +1,49 @@
 import "./index.scss"
+import { useState } from 'react';
 
 export default function Inicio() {
+    const [kilo, setKilo] = useState(0);
+    const [grm, setGrm] = useState(0);
 
+    function grama() {
 
-    return(
+        let res = kilo * 1000
+
+        setGrm(res)
+
+    }
+
+    return (
         <div className="pagina-inicio">
-            <header className="cabeçalho"> 
-            <img className="frei" src="/assets/images/logo_frei.png" alt="Frei" />
-            <h1>React FreiS</h1>
+            <header className="cabeçalho">
+                <div className="imh1">
+                    <img className="frei" src="/assets/images/logo_frei.png" alt="Frei" />
+                    <h1>React FreiS</h1>
+                </div>
+
+                <nav>
+                    <ul>
+                        <li><a href="#">Inicio</a></li>
+                        <li><a href="#">Sobre</a></li>
+                    </ul>
+                </nav>
 
             </header>
+            <section className="secao">
+                <h1>
+                    Exercício 02 - Converter Kg/gramas
+                </h1>
+                <div>
+                    <h2>Valor em gramas</h2>
+                    <input type="text" value={kilo} onChange={e => setKilo(e.target.value)} />
+                    <button onClick={grama}>Executar</button>
+                </div>
+                <div>
+                    o total é {grm} gramas
+                </div>
+            </section>
 
         </div>
     );
-    
+
 }
